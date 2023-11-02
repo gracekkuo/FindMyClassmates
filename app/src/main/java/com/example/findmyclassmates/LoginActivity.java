5ae6e5a8871fafd8b1da2ffde7f02fcdcfb6fd44
Grace Kuo <gracekuo@usc.edu>
2023-11-02 14:14:29 -0700

    update

--- app/src/main/java/com/example/findmyclassmates/LoginActivity.java
+++ app/src/main/java/com/example/findmyclassmates/LoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,8 +25,8 @@ public class LoginActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
 
-        Button loginButton = findViewById(R.id.loginButton);
 
+        Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,12 +34,31 @@ public class LoginActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                //login validation
-                //email use regex
-                //once everything is validated and response is 200.
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                if (!isValidEmail(email)) {
+                    // Show an error message for invalid email
+                    Toast.makeText(LoginActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
+                    return; // Don't proceed further
+                }
+
+                //password validation. if valid return 200
+
+                int responseCode = 200;
+                if (responseCode == 200) {
+                    // Start the MainActivity
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else {
+                    // Show an error for unsuccessful login
+                    Toast.makeText(LoginActivity.this, "Login failed. Please try again.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
+    private boolean isValidEmail(String email) {
+        // Regex pattern for basic email validation
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        // Check if the email matches the pattern
+        return email.matches(emailPattern);
     }
 }
